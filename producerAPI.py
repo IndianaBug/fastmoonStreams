8c895feef122a898db9cbbbf6a0c5cefca1d98a3
Pavlo Vanat <pvtoa@iscte-iul.pt>
2024-01-08 19:24:09 +0000

    'new'

--- producerAPI.py
+++ producerAPI.py
@@ -6,6 +6,7 @@ import json
 import random
 import ssl
 import aiohttp
+from urls import APIs
 
 def miliseconds_to_strftime(data) -> str:
     return datetime.utcfromtimestamp(int(data) / 1000.0).strftime('%Y-%m-%d %H:%M:%S UTC')
@@ -19,96 +20,23 @@ def generate_random_integer(n):
     return random_integer
 
 ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False  # Disable hostname checking
-ssl_context.verify_mode = ssl.CERT_NONE  # Disable certificate verification
+ssl_context.check_hostname = False  
+ssl_context.verify_mode = ssl.CERT_NONE  
 
-
-
-# to do ;
-# okx economic calendar
-# Coinbase websockets books. Needs authentication
 
 class combined_API():
-    """
-        Params :  { the length of the orderbook snapshot of ... 1 - 4,
 
-                    websocket uri for streaming
-
-                    Binance APIs : https://binance-docs.github.io/apidocs/spot/en/#change-log
-                                   https://binance-docs.github.io/apidocs/futures/en/#change-log
-                                   https://binance-docs.github.io/apidocs/delivery/en/#change-log
-                    OKEx APIs:     https://www.okx.com/docs-v5/en/?python#public-data-rest-api-get-instruments
-                    Bybit APIs:    https://bybit-exchange.github.io/docs/v5/intro
-                    Deribit APIs:  https://docs.deribit.com/#deribit-api-v2-1-1
-    """
-
-    def __init__(self, 
-                 binance_spot_books_usdt_snapshot_limit, # 1000 must e enough
-                 binance_spot_books_fdusd_snapshot_limit,
-                 binance_futures_books_usdt_snapshot_limit,
-                 binance_futures_books_usd_snapshot_limit,  
-                 websocket_uri
-                 ):
-
-        self.APIs = [
-
-            {"exchange":"binance", "insType":"SPOT", "obj":"depth", "instrument": "btc/usdt", "snapshotInterval":1, "url" : f"https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit={binance_spot_books_usdt_snapshot_limit}"},
-            {"exchange":"binance", "insType":"SPOT", "obj":"depth", "instrument": "btc/fdusd", "snapshotInterval":1, "url" : f"https://api.binance.com/api/v3/depth?symbol=BTCFDUSD&limit={binance_spot_books_fdusd_snapshot_limit}"},
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"depth", "instrument": "btc/usdt", "snapshotInterval":1, "url" : f"https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit={binance_futures_books_usdt_snapshot_limit}"},
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"depth", "instrument": "btc/usd", "snapshotInterval":1, "url" : f"https://dapi.binance.com/dapi/v1/depth?symbol=BTCUSD_PERP&limit={binance_futures_books_usd_snapshot_limit}"},
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"premiumIndex", "instrument": "btc/usdt", "snapshotInterval":300, "url" : "https://fapi.binance.com/fapi/v1/premiumIndex?symbol=BTCUSDT"}, # snap every 5 minute
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"fundingRate", "instrument": "btc/usd", "snapshotInterval":300, "url" : "https://dapi.binance.com/dapi/v1/fundingRate?symbol=BTCUSD_PERP"}, 
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"OI", "instrument": "btc/usdt", "snapshotInterval":3, "url" : "https://fapi.binance.com/fapi/v1/openInterest?symbol=BTCUSDT"},   # snap every 3 seconds
-            {"exchange":"binance", "insType":"PERPETUAL", "obj":"OI", "instrument": "btc/usd", "snapshotInterval":3, "url" : "https://dapi.binance.com/dapi/v1/openInterest?symbol=BTCUSD_PERP"},
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"TTA", "instrument": "btc/usdt", "snapshotInterval":300, "url" : "https://fapi.binance.com/futures/data/topLongShortAccountRatio?symbol=BTCUSDT&period=5m&limit=1"},   # snap every 5 minute
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"TTA", "instrument": "btc/usd", "snapshotInterval":300, "url" : "https://dapi.binance.com/futures/data/topLongShortAccountRatio?pair=BTCUSD&period=5m&limit=1"},
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"TTP", "instrument": "btc/usdt", "snapshotInterval":300, "url" : "https://fapi.binance.com/futures/data/topLongShortAccountRatio?symbol=BTCUSDT&period=5m&limit=1"},   # snap every 5 minute
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"TTP", "instrument": "btc/usd", "snapshotInterval":300, "url" : "https://dapi.binance.com/futures/data/topLongShortAccountRatio?pair=BTCUSD&period=5m&limit=1"},
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"GTA", "instrument": "btc/usdt", "snapshotInterval":300, "url" : "https://fapi.binance.com/futures/data/topLongShortAccountRatio?symbol=BTCUSDT&period=5m&limit=1"},   # snap every 5 minute
-            {"exchange":"binance", "insType":"STATISTIC_CONTRACTS", "obj":"GTA", "instrument": "btc/usd", "snapshotInterval":300, "url" : "https://dapi.binance.com/futures/data/topLongShortAccountRatio?pair=BTCUSD&period=5m&limit=1"},
-            {"exchange":"bybit", "insType":"STATISTIC_GENERAL", "obj":"insurace_fund", "instrument": "integrated", "snapshotInterval":300, "url" : "https://api.bybit.com/v5/market/insurance"},
-            {"exchange":"bybit", "insType":"STATISTIC_FUTURES", "obj":"position_Statistic", "instrument": "btc/usdt", "snapshotInterval":300, "url" : "https://api.bybit.com/v5/market/account-ratio?category=linear&symbol=BTCUSDT&period=1d&limit=50"},
-            {"exchange":"okx", "insType":"STATISTIC_SPOT", "obj":"TakerVolume", "instrument": "integrated", "snapshotInterval":300, "url" : f"https://www.okx.com/api/v5/rubik/stat/taker-volume?ccy=BTC&instType=SPOT&period=5m&begin={int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)}"}, # snap every 5 minute
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"TakerVolume", "instrument": "integrated", "snapshotInterval":300, "url" : f"https://www.okx.com/api/v5/rubik/stat/taker-volume?ccy=BTC&instType=CONTRACTS&period=5m&begin={int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)}"},
-            {"exchange":"okx", "insType":"STATISTIC_MARGIN", "obj":"CumLandingRatioBTC_to_quote", "snapshotInterval":300, "instrument": "integrated_BTC", "url" : f"https://www.okx.com/api/v5/rubik/stat/margin/loan-ratio?ccy=BTC&begin={int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)}"},
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"netLSratio", "snapshotInterval":300, "instrument": "integrated_BTC", "url" : f"https://www.okx.com/api/v5/rubik/stat/contracts/long-short-account-ratio?ccy=BTC&begin={int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)}"},
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"OIVolume_Fut_Perp", "snapshotInterval":300,"instrument": "integrated_BTC", "url" : f"https://www.okx.com/api/v5/rubik/stat/contracts/open-interest-volume?ccy=BTC&begin={int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)}"},
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"OIVolume_Options", "snapshotInterval":300,"instrument": "integrated_BTC", "url" : "https://www.okx.com/api/v5/rubik/stat/option/open-interest-volume?ccy=BTC"}, # every 8 hours. Descending order
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"Put_Call_Options", "snapshotInterval":300,"instrument": "integrated_BTC", "url" : "https://www.okx.com/api/v5/rubik/stat/option/open-interest-volume-ratio?ccy=BTC"}, # every 8 hours. Descending order
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"open-interest-volume-expiry", "snapshotInterval":300,"instrument": "integrated_BTC", "url" : "https://www.okx.com/api/v5/rubik/stat/option/open-interest-volume-expiry?ccy=BTC"}, # every 8 hours. Descending order
-            {"exchange":"okx", "insType":"STATISTIC_CONTRACTS", "obj":"taker_block_volume", "snapshotInterval":300, "instrument": "integrated_BTC", "url" : "https://www.okx.com/api/v5/rubik/stat/option/taker-block-volume?ccy=BTC"}, # every 8 hours. Descending order
-            # {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"calendar", "snapshotInterval":300, "instrument": "integrated", "url" : "https://www.okx.com/api/v5/public/economic-calendar"}, # Need authentication
-            {"exchange":"okx", "insType":"STATISTIC_MARGIN", "obj":"interest-rate-loan-quota", "snapshotInterval":300, "instrument": "integrated", "url" : "https://www.okx.com/api/v5/public/interest-rate-loan-quota"}, # Call every 8 hours
-            {"exchange":"okx", "insType":"STATISTIC_MARGIN", "obj":"vip-interest-rate-loan-quota", "snapshotInterval":300, "instrument": "integrated", "url" : "https://www.okx.com/api/v5/public/vip-interest-rate-loan-quota"}, 
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-MARGIN", "snapshotInterval":300, "instrument":"BTC", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=MARGIN&ccy=BTC&limit=1"},  # every 8 hours
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-MARGIN", "snapshotInterval":300, "instrument":"USDT", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=MARGIN&ccy=USDT&limit=1"},  # every 8 hours
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-SWAP", "snapshotInterval":300, "instrument":"BTC-USDT", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=SWAP&instFamily=BTC-USDT&limit=1"},
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-SWAP", "snapshotInterval":300, "instrument":"BTC-USD", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=SWAP&instFamily=BTC-USD&limit=1"},
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-FUTURES", "snapshotInterval":300, "instrument":"BTC-USDT", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=FUTURES&instFamily=BTC-USDT&limit=1"},
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-FUTURES", "snapshotInterval":300, "instrument":"BTC-USD", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=FUTURES&instFamily=BTC-USD&limit=1"},
-            {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"insurance-fund-OPTION", "snapshotInterval":300, "instrument":"BTC-USD", "url" : "https://www.okx.com/api/v5/public/insurance-fund?instType=OPTION&instFamily=BTC-USD&limit=1"},
-            {"exchange":"deribit", "insType":"FUTURES", "obj":"summary", "instrument":"BTC_integrated", "snapshotInterval":3,"url" : "wss://test.deribit.com/ws/api/v2",  "msg" : {"jsonrpc": "2.0", "id": generate_random_integer(10), "method": "public/get_book_summary_by_currency", "params": { "currency": "BTC", "kind": "future"}}},  # every 5 seconds
-            {"exchange":"deribit", "insType":"OPTIONS", "obj":"summary", "instrument":"BTC_integrated", "snapshotInterval":3,"url" : "wss://test.deribit.com/ws/api/v2",  "msg" : {"jsonrpc": "2.0", "id": generate_random_integer(10), "method": "public/get_book_summary_by_currency", "params": { "currency": "BTC", "kind": "option"}}},  # every 5 seconds
-            {"exchange":"deribit", "insType":"OPTIONS", "obj":"BVOL", "instrument":"BTC_integrated", "snapshotInterval":3,"url" : "wss://test.deribit.com/ws/api/v2",  "msg" : {"jsonrpc": "2.0", "id": generate_random_integer(10), "method": "public/get_volatility_index_data", "params": { "currency": "BTC", "start_timestamp": int(datetime.timestamp(datetime.now())), "end_timestamp": int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000), "resolution":"1"}}}
-        ]
-        self.websocket_uri = websocket_uri
-        self.fetched_API_calls_3 = {}
-        self.fetched_API_calls_300 = {}
-
-    def get_stream_names(self):
-        return [e["exchange"]+ "_" + e["insType"]+ "_" +e["obj"]+ "_" +e["instrument"] for e in self.APIs]
+    def __init__(self, APIs, producer):
+        self.API = APIs
+        self.producer = producer
     
     def get_stream_name(self, e):
         return e["exchange"]+ "_" +e["insType"]+ "_" +e["obj"]+ "_" +e["instrument"]
-
-    def miliseconds_to_strftime(self, data: int) -> str:
-        return datetime.utcfromtimestamp(data / 1000.0).strftime('%Y-%m-%d %H:%M:%S UTC')
 
     async def send_to_websocket(self, data):
         async with websockets.connect(self.websocket_uri) as websocket:
             await websocket.send(data) 
     
-
     async def fetch_data_websockets(self, data_dict, period):
         while True:
             async with websockets.connect(data_dict["url"],  ssl=ssl_context) as websocket:
@@ -127,21 +55,13 @@ class combined_API():
                     print(data.keys())
                     await asyncio.sleep(period)
 
-
     async def main(self):
-        
         tasks_3_deribit = [asyncio.ensure_future(self.fetch_data_websockets(x,  x["snapshotInterval"])) for x in self.APIs if x["exchange"] == "deribit" and x["snapshotInterval"] == 3 and x["obj"] != "depth"]
         tasks_3 = [asyncio.ensure_future(self.fetch_data(x,  x["snapshotInterval"])) for x in self.APIs if x["exchange"] != "deribit" and x["snapshotInterval"] == 3 and x["obj"] != "depth"]
         task_300 = [asyncio.ensure_future(self.fetch_data(x,   x["snapshotInterval"])) for x in self.APIs if x["exchange"] != "deribit" and x["snapshotInterval"] == 300 and x["obj"] != "depth"]
         tasks = tasks_3_deribit + tasks_3 + task_300
-        
         await asyncio.gather(*tasks)
     
-
-
-
-loader = combined_API(1000, 1000, 1000, 1000, "some_websocket")
-start_time = time.time()
+loader = combined_API(APIs, "producer")
 asyncio.run(loader.main())
-elapsed_time = time.time() - start_time
-print(f"Execution time: {elapsed_time} seconds")
+
--- producerWS.py
+++ producerWS.py
@@ -5,99 +5,19 @@ from aiokafka import AIOKafkaProducer
 from aiokafka.errors import KafkaStorageError
 import ssl
 import random
-
+from urls import WSs as wss 
+from urls import generate_random_integer
 
 ssl_context = ssl.create_default_context()
 ssl_context.check_hostname = False
 ssl_context.verify_mode = ssl.CERT_NONE
 
-def generate_random_integer(n):
-    if n <= 0:
-        raise ValueError("Length should be a positive integer")
-    lower_bound = 10 ** (n - 1)
-    upper_bound = (10 ** n) - 1
-    random_integer = random.randint(lower_bound, upper_bound)
-    return random_integer
-
 class WebSocketClient():
 
-    """     
-        Binance APIs : https://binance-docs.github.io/apidocs/spot/en/#change-log
-                       https://binance-docs.github.io/apidocs/futures/en/#change-log
-                       https://binance-docs.github.io/apidocs/delivery/en/#change-log
-        OKEx: https://www.okx.com/docs-v5/en/?python#public-data-websocket-funding-rate-channel
-        Bybit: https://www.bybit.com/future-activity/en/developer
-        Coinbase: https://docs.cloud.coinbase.com/exchange/docs/websocket-channels
-        Derebit: https://docs.deribit.com/
-    """
-
-
-    def __init__ (self, host, **kwards):
+    def __init__ (self, host, connection_data):
         
         self.host = host
-        self.connection_data = [
-             # Binance spot
-             { "n": "binance_spot_aggTrade_USDT", "e": "wss://stream.binance.com:9443/ws", "sn": "btcusdt@aggTrade"},  # ok
-             { "n": "binance_spot_depth_USDT", "e": "wss://stream.binance.com:9443/ws", "sn": "btcusdt@depth@1000ms"}, # ok
-             { "n": "binance_spot_aggTrade_FDUSD", "e": "wss://stream.binance.com:9443/ws", "sn": "btcfdusd@aggTrade"}, # ok
-             { "n": "binance_spot_depth_FDUSD", "e": "wss://stream.binance.com:9443/ws", "sn": "btcfdusd@depth@1000ms"}, # ok
-             # Binance perpetual usdt
-             { "n": "binance_perpetual_aggTrade_USDT", "e": "wss://fstream.binance.com/ws", "sn": "btcusdt@aggTrade"},  # ok
-             { "n": "binance_perpetual_depth_USDT", "e": "wss://fstream.binance.com/ws", "sn": "btcusdt@depth@500ms"}, # ok
-             { "n": "binance_perpetual_forceOrder_USDT", "e": "wss://fstream.binance.com/ws", "sn": "btcusdt@forceOrder"}, # ok
-             # Binance perpetual usd
-             { "n": "binance_perpetual_aggTrade_USD", "e": "wss://dstream.binance.com/ws", "sn": "btcusd_perp@aggTrade"}, # ok
-             { "n": "binance_perpetual_depth_USD", "e": "wss://dstream.binance.com/ws", "sn": "btcusd_perp@depth@500ms"},  # ok
-             { "n": "binance_perpetual_forceOrder_USD", "e": "wss://dstream.binance.com/ws", "sn": "btcusd_perp@forceOrder"}, # ok ? 
-             # Okx
-             { "n": "binance_perpetual_depth_USD", "e": "wss://ws.okx.com:8443/ws/v5/public", "sn": "btcusd_perp@forceOrder"},   
-             { "n" : "okx_",  # 11
-               "e":"wss://ws.okx.com:8443/ws/v5/public", 
-               "args" : [
-                 {"channel": "trades", "instId": "BTC-USDT-SWAP"}, # ok
-                 {"channel": "trades", "instId": "BTC-USD-SWAP"},  # ok
-                 {"channel": "trades", "instId": "BTC-USDT"},      # ok
-                 {"channel": "option-trades", "instType": "OPTION","instFamily": "BTC-USD"}, # ok
-                 {"channel": "books", "instId": "BTC-USDT-SWAP"}, # ok # Books 400 depth levels will be pushed in the initial full snapshot. Incremental data will be pushed every 100 ms for the changes in the order book during that period of time.
-                 {"channel": "books", "instId": "BTC-USD-SWAP"},  # ok
-                 {"channel": "books", "instId": "BTC-USDT"},      # ok
-                 {"channel": "public-struc-block-trades"}, # block trades https://www.okx.com/docs-v5/en/#block-trading-websocket-public-channel-public-structure-block-trades-channel
-                 {"channel": "open-interest", "instId": "BTC-USDT-SWAP"}, # ok
-                 {"channel": "open-interest", "instId": "BTC-USD-SWAP"},  # ok
-                 {"channel": "funding-rate", "instId": "BTC-USDT-SWAP"},  # ok
-                 {"channel": "funding-rate", "instId": "BTC-USD-SWAP"},   # ok
-                 {"channel": "opt-summary", "instFamily": "BTC-USD"},     # ok
-                 {"channel": "liquidation-orders", "instType": "SWAP"},   # ok
-                 {"channel": "liquidation-orders", "instType": "FUTURES"},# ok
-                 {"channel": "liquidation-orders", "instType": "MARGIN"}, # ok
-                 {"channel": "liquidation-orders", "instType": "OPTION"}, # ok
-                 {"channel": "adl-warning", "instType": "SWAP",  "instFamily": "BTC-USDT"},
-                 {"channel": "adl-warning", "instType": "FUTURES",  "instFamily": "BTC-USDT"},
-                 {"channel": "adl-warning", "instType": "OPTION",  "instFamily": "BTC-USDT"},
-                 {"channel": "adl-warning", "instType": "SWAP",  "instFamily": "BTC-USD"},
-                 {"channel": "adl-warning", "instType": "FUTURES",  "instFamily": "BTC-USD"},
-                 {"channel": "adl-warning", "instType": "OPTION",  "instFamily": "BTC-USD"}
-                                                        ]},
-             # bybit
-             { "n": "bybit_spot_depth_USDT", "e": "wss://stream.bybit.com/v5/public/spot", "sn": "orderbook.200.BTCUSDT"}, # ok
-             { "n": "bybit_spot_trades_USDT", "e": "wss://stream.bybit.com/v5/public/spot", "sn": "publicTrade.BTCUSDT"},  # ok
-             { "n": "bybit_perpetual_depth_USDT", "e": "wss://stream.bybit.com/v5/public/linear", "sn": "orderbook.200.BTCUSDT"},  # ok
-             { "n": "bybit_perpetual_trades_USDT", "e": "wss://stream.bybit.com/v5/public/linear", "sn": "publicTrade.BTCUSDT"},   # ok
-             { "n": "bybit_perpetual_forceOrder_USDT", "e": "wss://stream.bybit.com/v5/public/linear", "sn": "liquidation.BTCUSDT"},  # ok
-             { "n": "bybit_perpetual_tickers_USDT", "e": "wss://stream.bybit.com/v5/public/linear", "sn": "tickers.BTCUSDT"},  # ok # OI funding turnover etc
-             # coinbase
-             {"n":"coinbase_spot_btcusd", "e":"wss://ws-feed.exchange.coinbase.com", "sn": {"channels":"BTC-PERPETUAL"}},
-             # deribit
-             {"n":"deribit_", "e": "wss://test.deribit.com/ws/api/v2", "sn":{
-                "instrument_name" : "BTC-PERPETUAL"
-            }}
-
-
-                                                        ] 
-        # Economic calendar Retrieve the most up-to-date economic calendar data. This endpoint is only applicable to VIP 1 and above users in the trading fee tier.
-        # https://www.okx.com/docs-v5/en/#public-data-rest-api-get-economic-calendar-data
-        # You may want to nake an API call rather than websocker
-
+        self.connection_data = connection_data
 
     def parse_params(self, arg, exchange):
         if exchange == "binance": 
@@ -108,11 +28,6 @@ class WebSocketClient():
             return json.dumps({"op": "subscribe","args": [arg]})
         if exchange == "coinbase":
             return json.dumps(arg)
-        if exchange == "deribit":   # {"jsonrpc" : "2.0","id" : 9344, "method" : "public/get_book_summary_by_currency", "params" :
-            return json.dumps({"jsonrpc" : "2.0", "id" : 8106, "method" : "public/subscribe", "params" : {"channels":["chart.trades.BTC-PERPETUAL.1"]}})
-
-
-
 
     async def keep_alive(self, websocket, exchange, ping_interval=30):
         while True:
@@ -128,10 +43,7 @@ class WebSocketClient():
                     await websocket.send(json.dumps({"op": "ping"}))  
                 if exchange == "coinbase":
                     await asyncio.sleep(ping_interval - 10)
-                    await websocket.send(json.dumps({"op": "ping"}))
-                if exchange == "deribit":
-                    await asyncio.sleep(ping_interval - 10)
-                    await websocket.send(json.dumps({"op": "ping"}))                   
+                    await websocket.send(json.dumps({"op": "ping"}))               
             except websockets.exceptions.ConnectionClosed:
                 print("Connection closed. Stopping keep-alive.")
                 break
@@ -187,7 +99,7 @@ class WebSocketClient():
         #     await producer.stop()
 
 if __name__ == '__main__':
-    client = WebSocketClient(host='localhost:9092')
+    client = WebSocketClient('localhost:9092', wss)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(client.main())
