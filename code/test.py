ba7798f2c6807351123e334e9c34e108dc6ab492
Pavlo Vanat <136728020+badcoder-cloud@users.noreply.github.com>
2024-01-11 13:15:49 +0000

    coinbase ready

---
+++ code/test.py
@@ -0,0 +1,30 @@
+import requests
+from datetime import datetime, timedelta
+
+# Current time in milliseconds
+current_time_milliseconds = int(datetime.timestamp(datetime.now()) * 1000)
+
+# Time 5 minutes ago in milliseconds
+five_minutes_ago = datetime.now() - timedelta(minutes=5)
+five_minutes_ago_milliseconds = int(datetime.timestamp(datetime.now() - timedelta(minutes=5)) * 1000)
+
+print(five_minutes_ago_milliseconds)
+
+api_url = "wss://test.deribit.com/ws/api/v2/public"
+
+# Endpoint for book summary by currency
+endpoint = "/get_book_summary_by_currency"
+
+# Specify the currency pair (e.g., BTC-USD)
+currency_pair = "BTC-USD"
+
+a = {"exchange":"okx", "insType":"STATISTIC_GENERAL", "obj":"calendar", "snapshotInterval":300, "instrument": "integrated", "url" : "https://api.coinfeeds.io/coins/list"} # Need authentication
+
+
+r = requests.get(a['url'])
+if r.status_code == 200:
+    data = r.json()
+else:
+    data = r.status_code
+
+print(data)
