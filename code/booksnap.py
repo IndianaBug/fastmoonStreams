fc6885d605405f15690ae29a2e27e5acffa555db
Pavlo Vanat <136728020+badcoder-cloud@users.noreply.github.com>
2024-01-25 11:26:36 +0000

    Update booksnap.py

--- code/booksnap.py
+++ code/booksnap.py
@@ -8,12 +8,14 @@ def books_snapshot(exchange, instrument, insType, snaplength):
     """
       Gets full latest snapshot of limit orders.
     """
-    link = [x for x in apizzz if x["exchange"] = exchange and x["instrument"] = instrument and x["insType"] = insType]
-    try:
-        link = "&".join([link, f"limit={snaplength}"])
-        response = requests.get(link)
-    except:
-        get_books_snapshot(exchange, instrument, insType, snaplength-100)  # Say 5000 length is unavailable for a certain symbol, we will try with a smaller lrngth until we get the snapshot
+    link = [x for x in apizzz if x["exchange"] = exchange and x["instrument"] = instrument and x["insType"] = insType][0]
+    if exchange == "binance"
+        try:
+            link = "&".join([link, f"limit={snaplength}"])
+            response = requests.get(link)
+        except:
+            get_books_snapshot(exchange, instrument, insType, snaplength-100)  # Say 5000 length is unavailable for a certain symbol, we will try with a smaller lrngth until we get the snapshot
+    
     data = {
         "exchange" : exchange,
         "instrument" : instrument,
