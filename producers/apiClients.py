dd8d03a0fa8f1394c1c95d74657f9b4a3dd36ac9
Pavlo Vanat <136728020+badcoder-cloud@users.noreply.github.com>
2024-03-13 16:43:46 +0000

    added abstractions of coinbase, bybit and okx api calls

--- producers/apiClients.py
+++ producers/apiClients.py
@@ -1,15 +1,25 @@
 import requests
 import aiohttp
+import http
 import asyncio
 import websockets
 import time
 import json
 import re
 import ssl
+from cryptography.hazmat.primitives import serialization
+import jwt
+import secrets
+import urllib.parse
+
+
+
 
 ssl_context = ssl.create_default_context()
 ssl_context.check_hostname = False
 ssl_context.verify_mode = ssl.CERT_NONE
+
+# Exchnages than require to fetch books before : binance, bybit
 
 
 class CommunicationsManager:
@@ -22,7 +32,7 @@ class CommunicationsManager:
             response = requests.get(connection_data.get("url"), params=connection_data.get("params"), headers=connection_data.get("headers"))
             if response.status_code == 200:
                 return {
-                    "instrument" : connection_data.get("instrument"),
+                    "instrument" : connection_data.get("instrumentName"),
                     "exchange" : connection_data.get("exchange"),
                     "insType" : connection_data.get("insTypeName").lower(),
                     "response" : response.json() ,
@@ -35,8 +45,24 @@ class CommunicationsManager:
     
     @classmethod
     def make_httpRequest(cls, connection_data):
-        pass
-
+        conn = http.client.HTTPSConnection(connection_data.get("endpoint"))
+        basepoint = "?".join([connection_data.get("basepoint"), urllib.parse.urlencode(connection_data.get("params"))])
+        print(basepoint)
+        conn.request(
+            "GET", 
+            basepoint, 
+            connection_data.get("payload"), 
+            connection_data.get("headers"),
+            )
+        res = conn.getresponse()
+        response = json.loads(res.read())
+        return {
+            "instrument" : connection_data.get("instrumentName"),
+            "exchange" : connection_data.get("exchange"),
+            "insType" : connection_data.get("insTypeName").lower(),
+            "response" : response,
+        }
+        
 
 # For gate
 # headers = info["headers"]
@@ -218,9 +244,10 @@ class bybit(CommunicationsManager):
         "depth" : "/v5/market/orderbook",
         "gta" : "/v5/market/account-ratio",
         "oi" : "/v5/market/tickers"
-    },
+    }
     instrument_category_mapping = {
         "spot" : "spot",
+        "perp" : "linear",
         "derivate" : "linear",
         "option" : "option",
     }
@@ -231,7 +258,7 @@ class bybit(CommunicationsManager):
             },
         "gta" : {
             "symbol": {"type": str, "default": ""},
-            "period": {"type": str, "default": "5m"},
+            "period": {"type": str, "default": "1d"},
             "limit": {"type": int, "default": 1},
                 },
         "oi": {
@@ -242,8 +269,8 @@ class bybit(CommunicationsManager):
     @classmethod
     def buildRequest(cls, insType:str, objective:str, maximum_retries:int=10, books_dercemet:int=100, **kwargs)->dict: 
         """
-            instrument : ex btcusdt for spot/coin-m 
-                            brcusd_perp or bnb_perp or btcusd_4145 for coin-c
+            instrument : ex btcusdt btc_12313 for spot/derivative
+                            btc for option
             insType : spot, derivate, option
             objective :  depth, gta, oi (from tickers, for options)
             Maxium retries of an API with different parameters in case API call is impossible. Useful when you cant catch the limit
@@ -251,14 +278,23 @@ class bybit(CommunicationsManager):
             **kwargs : request parameters
         """
         params = dict(kwargs)
-        params["symbol"] = params["symbol"].upper()
-        instrument = params.get("symbol")
-        # is it spot, perp or vanilla future?
-        insTypeName = "future" if bool(re.search(r'\d', instrument)) else "perp"
-        insTypeName = "spot" if insType=="spot" else insTypeName
+        if insType == "option":
+            params["baseCoin"] = params["baseCoin"].upper()
+            instrument = params.get("baseCoin")
+        else:
+            params["symbol"] = params["symbol"].upper()
+            instrument = params.get("symbol")
+        # is it spot, perp or vanilla future, or option?
+        if bool(re.search(r'\d', instrument)) and insType=="derivate":
+            insTypeName = "future"
+        if bool(re.search(r'\d', instrument)) is False and insType=="derivate":
+            insTypeName = "perp"
+        if insType in ["spot", "option"]:
+            insTypeName = insType
         # create url        
         endpoint = cls.endpoint
         basepoint = cls.basepoints.get(objective)
+        params["category"] = cls.instrument_category_mapping.get(insType)
         url = endpoint + basepoint
         # verify params
         headers = {}
@@ -269,7 +305,7 @@ class bybit(CommunicationsManager):
             "headers" : headers, 
             "instrumentName" : instrument.lower(), 
             "insTypeName" : insTypeName.lower(), 
-            "exchange" : "binance", 
+            "exchange" : "bybit", 
             "repeat_code" : cls.repeat_response_code,
             "maximum_retries" : maximum_retries, 
             "books_dercemet" : books_dercemet
@@ -286,15 +322,192 @@ class bybit(CommunicationsManager):
         connection_data = cls.buildRequest(*args, **kwargs)
         response = await cls.make_aiohttpRequest(connection_data)
         return response
+    
+
+### OKX ###
+
+class okx(CommunicationsManager):
+    """
+        Abstraction of bybit api calls
+    """
+    repeat_response_code = -1130
+    endpoint = "https://www.okx.com"
+    basepoints = {
+        "gta" : "/api/v5/rubik/stat/contracts/long-short-account-ratio",
+        "oi" : "/api/v5/public/open-interest"
+    }
+    default_params  = {
+        "gta" : {
+            "ccy": {"type": str, "default": "BTC"},
+            "period": {"type": str, "default": "5m"},
+                },
+        "oi": {
+            "instType" : {"type": str, "default": "OPTION"},
+            "instFamily" : {"type": str, "default": "BTC-USD"},
+        }
+        }
+
+    @classmethod
+    def buildRequest(cls, objective:str, maximum_retries:int=10, books_dercemet:int=100, **kwargs)->dict: 
+        """
+            objective :  gta, oi 
+            **kwargs : request parameters
+        """
+        params = dict(kwargs)
+        if objective == "gta":
+            insTypeName = "perp"
+        if objective != "gta":
+            if params["instType"].upper() == "SWAP":
+                insTypeName = "perp"
+            if params["instType"].upper() == "OPTION":
+                insTypeName = "option"
+            if params["instType"].upper() == "FUTURES":
+                insTypeName = "future"
+            if params["instType"].upper() == "SPOT":
+                insTypeName = "spot"
+        
+        for key in ["ccy", "instType", "instFamily"]:
+            if key in params:
+                params[key] = params[key].upper()
+        
+        if objective == "gta":
+            instrument = params.get("ccy")
+        if objective == "oi":
+            instrument = "".join(params.get("instFamily").split("-"))
+
+   
+        endpoint = cls.endpoint
+        basepoint = cls.basepoints.get(objective)
+        url = endpoint + basepoint
+        headers = {}
+        return {
+            "url" : url, 
+            "objective" : objective,
+            "params" : params, 
+            "headers" : headers, 
+            "instrumentName" : instrument.lower(), 
+            "insTypeName" : insTypeName.lower(), 
+            "exchange" : "okx", 
+            "repeat_code" : cls.repeat_response_code,
+            "maximum_retries" : maximum_retries, 
+            "books_dercemet" : books_dercemet
+            }
+
+    @classmethod
+    def fetch(cls, *args, **kwargs):
+        connection_data = cls.buildRequest(*args, **kwargs)
+        response = cls.make_request(connection_data)
+        return response
+
+    @classmethod
+    async def aiohttpFetch(cls, *args, **kwargs):
+        connection_data = cls.buildRequest(*args, **kwargs)
+        response = await cls.make_aiohttpRequest(connection_data)
+        return response
+    
+
+### COINBASE ###
+
+
+class coinbase(CommunicationsManager):
+    """
+        Abstraction of bybit api calls
+    """
+
+    def __init__ (self, apikey, secretkey):
+        self.apikey = apikey
+        self.secretkey = secretkey
+        self.repeat_response_code = -1130
+        self.endpoint = "api.coinbase.com"
+        self.basepoints = {
+            "depth" : "/api/v3/brokerage/product_book",
+        }
+        self.default_params  = {
+            "depth" : {
+                "product_id": {"type": str, "default": "BTC-USD"},
+                    },
+            }
+    def buildRequest(self, objective:str, maximum_retries:int=10, books_dercemet:int=100, **kwargs)->dict: 
+        """
+            instrument : ex: BTC-USD
+            objective :  depth
+            # OMIT Maxium retries of an API with different parameters in case API call is impossible. Useful when you cant catch the limit
+            # OMIT books_dercemet : the derement of books length in case of api call being unsuccessful. If applicable
+            **kwargs : request parameters
+        """
+        params = dict(kwargs)
+        instrument = params.get("product_id")
+        insTypeName = "spot"
+        endpoint = self.endpoint
+        basepoint = self.basepoints.get(objective)
+        payload = ''
+        headers = {
+            "Authorization": f"Bearer {self.build_jwt(objective)}",
+            'Content-Type': 'application/json'
+        }
+        return {
+            "endpoint" : endpoint, 
+            "basepoint" : basepoint,
+            "objective" : objective,
+            "params" : params, 
+            "headers" : headers, 
+            "instrumentName" : instrument.replace("-", ""),
+            "insTypeName" : insTypeName, 
+            "exchange" : "coinbase", 
+            "repeat_code" : self.repeat_response_code,
+            "maximum_retries" : maximum_retries, 
+            "books_dercemet" : books_dercemet,
+            "payload" : payload
+            }
+    
+    def build_jwt(self, objective):
+        if objective == "depth":
+            objectiveCoinbase = "product_book"
+        key_name       =  self.apikey
+        key_secret     =  self.secretkey
+        request_method = "GET"
+        request_host   = "api.coinbase.com"
+        request_path   = f"/api/v3/brokerage/{objectiveCoinbase}"
+        service_name   = "retail_rest_api_proxy"
+        private_key_bytes = key_secret.encode('utf-8')
+        private_key = serialization.load_pem_private_key(private_key_bytes, password=None)
+        uri = f"{request_method} {request_host}{request_path}"
+        jwt_payload = {
+            'sub': key_name,
+            'iss': "coinbase-cloud",
+            'nbf': int(time.time()),
+            'exp': int(time.time()) + 120,
+            'aud': [service_name],
+            'uri': uri,
+        }
+        jwt_token = jwt.encode(
+            jwt_payload,
+            private_key,
+            algorithm='ES256',
+            headers={'kid': key_name, 'nonce': secrets.token_hex()},
+        )
+        return jwt_token
+
+    def fetch(self, *args, **kwargs):
+        connection_data = self.buildRequest(*args, **kwargs)
+        response = CommunicationsManager.make_httpRequest(connection_data)
+        return response
 
 
 
 
-async def example():
-    a = await bybit.aiohttpFetch("derivate", "depth", symbol="btcusdt", limit=50)
-    print(a)
 
-asyncio.run(example())
+
+
+# c = coinbase(coinbaseAPI, coinbaseSecret)
+
+# print(c.fetch("depth", product_id="BTC-USD"))
+
+# async def example():
+#     a = await okx.aiohttpFetch("gta", period="5m", ccy="BTC")
+#     print(a)
+
+# asyncio.run(example())
     
     # @classmethod
     # @APIClientDecorator.api_request_decorato
@@ -325,7 +538,6 @@ asyncio.run(example())
 #         "spot" : "https://api.huobi.pro",
 #         "perp" : "https://api.hbdm.com",
 #     },
-#     "bybit" :  "https://api.bybit.com",
 #     "coinbase" : "api.coinbase.com",
 #     "gateio" : "https://api.gateio.ws",
 #     "bitget" : "https://api.bitget.com",7
@@ -335,20 +547,6 @@ asyncio.run(example())
 # }
 
 # APIbasepoints = {
-#     "binance" : {
-#         "spot" : {
-#             "depth" : "/api/v3/depth",
-#         },
-#         "perp" : {
-#             "mfutures" : {
-#                 "depth" : "/fapi/v1/depth",
-#             },
-#             "cfutures" : {
-#                 "depth" : "/dapi/v1/depth",
-
-#             },
-#         }
-#     },
 #     "kucoin" : {
 #         "spot" : "https://api.kucoin.com", 
 #         "perp" : "https://api-futures.kucoin.com",
@@ -364,12 +562,10 @@ asyncio.run(example())
 #     "bybit" :  {
 #         "depth" : "/v5/market/orderbook",
 #     },
-#     "coinbase" : "api.coinbase.com",
 #     "gateio" : "https://api.gateio.ws",
 #     "bitget" : "https://api.bitget.com",
 #     "deribit" : "wss://test.deribit.com",
 #     "bingx" : "https://open-api.bingx.com",
-#     "okx" : "https://www.okx.com",
 # }
 
 
